8ef26af95fd738f37c51caed06e004af0c57cca3
Jose Reyes <josereyes.jdi@gmail.com>
2016-06-16 12:42:14 -0500

    Actualizar carpetas

--- 04-Class/01.js
+++ 04-Class/01.js
@@ -2,15 +2,22 @@
 <head>
 </head>
 <body>
+
 <script type="text/javascript">
+  var serie;
+  serie=11;
   var x;
   x=1;
-  while (x<=100)
+  while (x<=25)
   {
-    document.write(x);
+    document.write(serie);
     document.write('<br>');
     x=x+1;
+    serie=serie+11;
   }
 </script>
+
 </body>
 </html>
+
+// JavaScript Document
