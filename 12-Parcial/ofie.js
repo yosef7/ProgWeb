61b0d77c86c650c35cca62a949c6086f6f236f9b
Jose Reyes <josereyes.jdi@gmail.com>
2016-07-28 01:12:05 -0500

    Parcial, clase 12

---
+++ 12-Parcial/ofie.js
@@ -0,0 +1,84 @@
+var sexo, peso, edad, nombre, estatura;
+do {
+    sexo = prompt('Ingrese el sexo del niño (1) o niña (2):', '');
+    sexo = parseInt(sexo);
+
+                }
+                1 = niño
+                document.write('El sexo del niño o niña es ' + sexo + '  ');
+                if (sexo = niña) {
+                    document.write('Tiene 1 digitos');  }
+
+                else {
+                    if ((estatura >= 1.60)&&(peso >= 145))  {
+                        document.write('Esta niña es apta para jugar basquetbol.');
+                    } else {
+                        document.write('Esta niña NO es apta para jugar basquetbol.');
+                    }
+                }
+
+                document.write('<br>');
+            } while (valor != 0);
+
+            while (i <= 30) {
+                text += "The number is " + i;
+                i++;
+            }
+
+
+
+
+do {
+    i++;
+}
+while (i <= 30);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://1buenanuevas.wordpress.com/2011/08/28/3-estructuras-condicionales-if-for-while-do-while-switch/
+ESTRUCTURA
+
+if (condición) {
+código del programa con la condición verdadera
+}
+else {
+código del programa cuando la condición no se cumpla.
+}
+
+
+
+
+
+
+
+
+    var sx;
+
+
+    ifs {
+      sexo = promt('Ingrese el sexo del estudiante {Masculino (1), Femenino (2)} ','');
+      sexo=parseInt(sexo);
+
+
+      while (sexo==1); {
+
+      } () {
+        document.write("El sexo del estudiantes es Masculino."+ sexo);
+      } else {
+        document.write("El sexo del estudiantes es Femenino."+ sexo);
+    }
